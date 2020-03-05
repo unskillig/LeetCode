75efe33c653f1edff21ff3f424e5a81025f4b4f8
DWP6294 <DWP6294@DPN40116498.dwpbank-prod.domint>
2020-03-05 07:34:45 +0100

    Result

---
+++ AbstractFactoryPattern/src/main/java/Color.java
@@ -0,0 +1,5 @@
+
+public abstract class Color {
+
+	public abstract String getColour();
+}
---
+++ AbstractFactoryPattern/src/main/java/Dog.java
@@ -0,0 +1,16 @@
+
+public class Dog extends Animal{
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return "Dog";
+	}
+
+	@Override
+	public String makeSound() {
+		// TODO Auto-generated method stub
+		return "Wuff";
+	}
+
+}
